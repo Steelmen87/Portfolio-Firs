647b0f367d9209b15ee414d6d2797c646d76952b
Steel <Steelmen87@bk.ru>
2024-02-07 14:19:50 +0300

    add Main Style

--- src/components/FlexWrapper.ts
+++ src/components/FlexWrapper.ts
@@ -8,8 +8,9 @@ type FlexWrapperProps = {
 }
 export const FlexWrapper = styled.div<FlexWrapperProps>`
     display: flex;
-    flex-direction: ${props=>props.direction || "row"};
-    justify-content: ${props=>props.justify || "flex-start"};
-    align-items: ${props=>props.align || "stretch"};
-    flex-wrap: ${props=>props.wrap || "nowrap"};
+    flex-direction: ${props => props.direction || "row"};
+    justify-content: ${props => props.justify || "flex-start"};
+    align-items: ${props => props.align || "stretch"};
+    flex-wrap: ${props => props.wrap || "nowrap"};
+    height: 100%;
 `
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,28 +1,50 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, {useTheme} from "styled-components";
 import photo from './../../../assets/images/Photo.png'
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
 
 export const Main = () => {
+    const theme = useTheme()
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <span>  Hi There  </span>
-                    <Name> Iam Petrov Konstantin </Name>
-                    <MainTitle> A Web Developer </MainTitle>
-                </div>
-                <StyledPhoto src={photo}/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-between"} theme={theme}>
+                    <div>
+                        <SmallText> Hi There </SmallText>
+                        <Name> I am <span>Petrov Konstantin</span></Name>
+                        <MainTitle> A Web Developer </MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo}/>
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
 const StyledMain = styled.section`
     min-height: 100vh;
-    background-color: #fffe7;
+    display: flex;
 `
 
-const StyledPhoto = styled.img`
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+
+    &::before {
+        content: "";
+        width: 320px;
+        height: 450px;
+        border: 5px solid ${props => props.theme.colors.accent};
+
+        position: absolute;
+        top: -24px;
+        left: 20px;
+        z-index: -1;
+    }
+`
+const Photo = styled.img`
     width: 300px;
     height: 400px;
     object-fit: cover;
@@ -30,11 +52,39 @@ const StyledPhoto = styled.img`
 const MainTitle = styled.h1`
     font-size: 27px;
     font-weight: 400;
-    line-height: 41px;
     letter-spacing: 0;
-    text-align: left;
+
 
 
 `
 
-const Name = styled.h2``
+const Name = styled.h2`
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 50px;
+    font-weight: 700;
+    letter-spacing: 0.05em;
+    margin: 10px 0;
+
+    span {
+        position: relative;
+        z-index: 0;
+
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${props => props.theme.colors.accent};
+
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+`
+
+const SmallText = styled.span`
+    font-size: 14px;
+    font-weight: 400;
+
+`
